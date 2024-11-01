aeb2831f60b943bc9b3d2a45a7d074acabc49fcd
Tra8der <ywlee8888@gmail.com>
2024-11-01 23:03:20 +0800

    About

--- components/services/contactUs.tsx
+++ components/services/contactUs.tsx
@@ -30,7 +30,7 @@ const ContactUs: React.FC<SidebarProps> = ({ isOpen, toggleSidebar }) => {
         className="h-full w-full"
         style={{
           height: "auto",
-          minHeight: "100vh",
+          minHeight: "80vh",
           backgroundImage: "url('/img/specialist_background.png')", // Use your background image path
           backgroundSize: "cover",
           backgroundPosition: "center",
