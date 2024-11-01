8f7d2aa1eb9835a1094530d8a68a445e4dddc24b
Tra8der <ywlee8888@gmail.com>
2024-11-01 22:48:56 +0800

    Update About

--- components/services/contactUs.tsx
+++ components/services/contactUs.tsx
@@ -21,7 +21,8 @@ const ContactUs: React.FC<SidebarProps> = ({ isOpen, toggleSidebar }) => {
       className="fixed right-0 z-50 transition-transform duration-300"
       style={{
         top: "140px",
-        minHeight: "100vh",
+        height: "auto", // Use auto to adjust based on content
+        minHeight: "100vh", // Ensures at least full viewport height
         backgroundImage: "url('/img/specialist_background.png')", // Set your background image
         backgroundSize: "cover", // Ensure the image covers the entire sidebar
         backgroundPosition: "center", // Center the background image
