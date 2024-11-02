8650d86871283047d62cb4bb2784afccdca40f14
Tra8der <ywlee8888@gmail.com>
2024-11-02 15:20:04 +0800

    Revert About Us Contact

--- components/services/contactUs.tsx
+++ components/services/contactUs.tsx
@@ -21,14 +21,13 @@ const ContactUs: React.FC<SidebarProps> = ({ isOpen, toggleSidebar }) => {
       style={{
         top: "140px",
         height: "auto",
-        width: "60rem",
+        width: "100%", // Use 100% width for mobile view
       }}
     >
       {/* Background image covering the entire sidebar */}
       <div
         className="h-full w-full"
         style={{
-          height: "auto",
           minHeight: "65vh",
           backgroundImage: "url('/img/specialist_background.png')",
           backgroundSize: "cover",
@@ -46,7 +45,7 @@ const ContactUs: React.FC<SidebarProps> = ({ isOpen, toggleSidebar }) => {
         {/* Close button */}
         <button
           onClick={toggleSidebar}
-          className="bg-green-500 text-white z-20 absolute left-[-3.5rem] top-4"
+          className="bg-green-500 text-white z-20 absolute left-[-3.5rem] top-4 md:left-[-2rem] md:top-8" // Adjust position for mobile
           style={{
             fontSize: "2rem",
             padding: "0.5rem 0.75rem",
@@ -70,19 +69,19 @@ const ContactUs: React.FC<SidebarProps> = ({ isOpen, toggleSidebar }) => {
             <div className="absolute inset-0 bg-black bg-opacity-40 rounded-md"></div>
 
             {/* "Get in touch" text over image */}
-            <h1 className="absolute inset-0 flex items-center text-white text-5xl font-bold left-[5%]">
+            <h1 className="absolute inset-0 flex items-center text-white text-3xl md:text-5xl font-bold left-[5%]">
               Get in touch
             </h1>
-            <p className="absolute inset-0 flex items-center text-white text-lg font-bold left-[5%] top-[110px]">
+            <p className="absolute inset-0 flex items-center text-white text-base md:text-lg font-bold left-[5%] top-[110px]">
               Want to get in touch? We'd love to hear from you. <br /> Here's how
               you can reach us.
             </p>
           </div>
 
           {/* Contact Cards positioned above the image */}
-          <div className="absolute top-5/8 transform -translate-y-1/2 w-full flex justify-between gap-4 mb-8 px-4">
+          <div className="absolute top-5/8 transform -translate-y-1/2 w-full flex flex-col md:flex-row justify-between gap-4 mb-8 px-4">
             {/* Card 1 */}
-            <div className="bg-gray-100 p-6 rounded-lg shadow-md w-1/2">
+            <div className="bg-gray-100 p-6 rounded-lg shadow-md w-full md:w-1/2">
               <h2 className="text-xl font-bold mb-4">Membership Services</h2>
               <div style={{ display: "flex", alignItems: "center" }}>
                 <FaWhatsapp style={{ marginRight: "8px", fontSize: "24px" }} />
@@ -99,7 +98,7 @@ const ContactUs: React.FC<SidebarProps> = ({ isOpen, toggleSidebar }) => {
             </div>
 
             {/* Card 2 */}
-            <div className="bg-gray-100 p-6 rounded-lg shadow-md w-1/2">
+            <div className="bg-gray-100 p-6 rounded-lg shadow-md w-full md:w-1/2">
               <h2 className="text-xl font-bold mb-4">Healthcare Provider</h2>
               <div style={{ display: "flex", alignItems: "center" }}>
                 <FaWhatsapp style={{ marginRight: "8px", fontSize: "24px" }} />
