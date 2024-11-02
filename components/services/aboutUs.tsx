611f61de240131091f9400b6dd5a4311922eb182
Tra8der <ywlee8888@gmail.com>
2024-11-02 15:40:04 +0800

    Update About Us

--- components/services/aboutUs.tsx
+++ components/services/aboutUs.tsx
@@ -22,7 +22,7 @@ const AboutUs: React.FC<SidebarProps> = ({ isOpen, toggleSidebar }) => {
   return (
     <div
       ref={sidebarRef}
-      className="fixed right-0 z-50 transition-transform duration-300 p-8 md:w-[60rem] w-full md:h-auto h-screen"
+      className="fixed right-0 z-50 transition-transform duration-300 p-8 md:w-[60rem] w-[90%] sm:w-[80%] h-auto md:h-auto h-screen"
       style={{
         top: "190px",
         backgroundImage: "url('/img/specialist_background.png')",
@@ -32,7 +32,13 @@ const AboutUs: React.FC<SidebarProps> = ({ isOpen, toggleSidebar }) => {
       }}
     >
       {/* Semi-transparent overlay to create a mask effect */}
-      <div className="absolute inset-0" style={{ backgroundColor: "rgba(129, 216, 208, 0.8)", borderRadius: "8px" }}></div>
+      <div
+        className="absolute inset-0"
+        style={{
+          backgroundColor: "rgba(129, 216, 208, 0.8)",
+          borderRadius: "8px",
+        }}
+      ></div>
 
       {/* Close button */}
       <button
@@ -51,27 +57,32 @@ const AboutUs: React.FC<SidebarProps> = ({ isOpen, toggleSidebar }) => {
         <img
           src="/img/specialistasia_logo_banner_black.png"
           alt="About Specialist Asia"
-          className="mb-8 max-w-md h-auto"
+          className="mb-8 max-w-[70%] h-auto"
         />
         <h1 className="text-2xl font-bold mb-3">About Specialist Asia</h1>
         <p className="text-lg mb-5">
-          <strong>Specialist Asia</strong> offers exclusive membership-based healthcare concierge service, designed to provide
-          personalized support and priority access to world-class healthcare.
+          <strong>Specialist Asia</strong> offers exclusive membership-based
+          healthcare concierge service, designed to provide personalized support
+          and priority access to world-class healthcare.
         </p>
         <p className="text-lg mb-3">
-          Our team of dedicated experts ensures that all the details,
-          including healthcare coordination, translations, and local liaisons, are managed seamlessly.
+          Our team of dedicated experts ensures that all the details, including
+          healthcare coordination, translations, and local liaisons, are managed
+          seamlessly.
         </p>
         <p className="text-lg mb-3">
-          As a member, you gain tailored assistance with everything from pre-trip healthcare consultations to travel arrangements,
-          appointment scheduling, and post-treatment follow-ups.
+          As a member, you gain tailored assistance with everything from
+          pre-trip healthcare consultations to travel arrangements, appointment
+          scheduling, and post-treatment follow-ups.
         </p>
         <p className="text-lg mb-3">
-          Whether for consultation or treatment, our local experts ensure every step of your journey is smooth, allowing
-          you to focus solely on recovery and well-being.
+          Whether for consultation or treatment, our local experts ensure every
+          step of your journey is smooth, allowing you to focus solely on
+          recovery and well-being.
         </p>
         <p className="text-lg mb-5">
-          Seamlessly navigate the complexities of traveling overseas for healthcare treatment across Asia.
+          Seamlessly navigate the complexities of traveling overseas for
+          healthcare treatment across Asia.
         </p>
       </div>
     </div>
