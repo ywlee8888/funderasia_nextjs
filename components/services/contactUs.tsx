f199dfaf16cf8b400120c0626e643b265d7aa779
Tra8der <ywlee8888@gmail.com>
2024-11-03 11:09:56 +0800

    Changes

--- components/services/contactUs.tsx
+++ components/services/contactUs.tsx
@@ -84,11 +84,11 @@ const ContactUs: React.FC<SidebarProps> = ({ isOpen, toggleSidebar }) => {
           {/* Contact Cards positioned above the image */}
           {/*<div className="absolute top-5/8 transform -translate-y-1/2 w-full flex justify-between gap-4 mb-8 px-4">*/}
           {/*<div className="absolute top-5/8 transform -translate-y-1/2 w-full flex flex-col md:flex-row justify-between gap-4 mb-8 px-4">*/}
-          <div className="top-5/8 transform -translate-y-1/2 w-full flex flex-col md:flex-row justify-between gap-4 mt-4 mb-8 px-4">
+          <div className="top-5/8 transform -translate-y-1/2 flex flex-col md:flex-row justify-between gap-4 mt-4 mb-8 px-4">
             {/* Card 1 */}
             {/*<div className="bg-gray-100 p-6 rounded-lg shadow-md w-1/2">*/}
             {/*<div className="bg-gray-100 p-6 rounded-lg shadow-md w-full md:w-3/4">*/}
-            <div className="relative bg-gray-100 p-6 rounded-lg shadow-md w-full md:w-3/4 overflow-hidden">
+            <div className="relative bg-gray-100 p-6 rounded-lg shadow-md w-full overflow-hidden">
               <h2 className="text-xl md:text-2xl font-bold mb-4">
                 Membership Services
               </h2>
