3bbfff84d14d58338b9fcc6eaa9035ee0fdd5ad0
Tra8der <ywlee8888@gmail.com>
2025-03-18 16:01:23 +0800

    Update Scale

--- components/banners/bannerfull.tsx
+++ components/banners/bannerfull.tsx
@@ -52,11 +52,11 @@ const FullWidthImage: React.FC<FullWidthImageProps> = ({ src, alt }) => {
         </button>
       </div>
       <div className={styles.overlayCard}>
-        <div className="hidden md:flex justify-center">
+        <div className="hidden md:flex justify-center scale-70">
           <img
             src="/img/specialistasia_logo_banner_black.png"
             alt="About Specialist Asia"
-            className="mb-3 scale-70 !important"
+            className="mb-3"
           />
         </div>
         <div className={styles.cardTitle}>
