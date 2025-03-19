ea94eaf67dea4b81a06c27fd3e1359d0c530578e
Tra8der <ywlee8888@gmail.com>
2025-03-19 09:44:13 +0800

    Update WA Z-Index

--- components/chat/whatsapp.tsx
+++ components/chat/whatsapp.tsx
@@ -1,19 +1,19 @@
-import React from 'react';
+import React from "react";
 
 const WhatsApp: React.FC = () => {
   const whatsappLink = "https://wa.me/6584118213";
 
   return (
-    <a 
-      href={whatsappLink} 
-      target="_blank" 
+    <a
+      href={whatsappLink}
+      target="_blank"
       rel="noopener noreferrer"
-      className="fixed bottom-4 left-4 z-1000 animate-bounce"
+      className="fixed bottom-4 left-4 z-[10000] animate-bounce"
     >
-      <img 
-        src="/img/whatsapp-chat.png" 
-        alt="Chat on WhatsApp" 
-        className="w-40 h-auto sm:w-44 md:w-48 lg:w-64 hover:scale-110 transition-transform duration-300 z-1000"
+      <img
+        src="/img/whatsapp-chat.png"
+        alt="Chat on WhatsApp"
+        className="w-40 h-auto sm:w-44 md:w-48 lg:w-64 hover:scale-110 transition-transform duration-300"
       />
     </a>
   );
