0c060a09d282f3557bce0caf99db0a9c6f0ca1ed
Tra8der <ywlee8888@gmail.com>
2025-03-19 09:36:26 +0800

    Update WA Icon Z-Index

--- components/chat/whatsapp.tsx
+++ components/chat/whatsapp.tsx
@@ -8,12 +8,12 @@ const WhatsApp: React.FC = () => {
       href={whatsappLink} 
       target="_blank" 
       rel="noopener noreferrer"
-      className="fixed bottom-4 left-4 z-200 animate-bounce"
+      className="fixed bottom-4 left-4 z-1000 animate-bounce"
     >
       <img 
         src="/img/whatsapp-chat.png" 
         alt="Chat on WhatsApp" 
-        className="w-40 h-auto sm:w-44 md:w-48 lg:w-64 hover:scale-110 transition-transform duration-300"
+        className="w-40 h-auto sm:w-44 md:w-48 lg:w-64 hover:scale-110 transition-transform duration-300 z-1000"
       />
     </a>
   );
