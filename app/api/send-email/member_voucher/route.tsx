e715c3a67dc1c0b65983c8eaa38c48c2a33822fa
Tra8der <ywlee8888@gmail.com>
2025-11-13 16:24:01 +0800

    Update

--- app/api/send-email/member_voucher/route.tsx
+++ app/api/send-email/member_voucher/route.tsx
@@ -42,6 +42,7 @@ export async function POST(req: NextRequest) {
     const mailOptions = {
       from: "member@specialist.asia",
       to: email,
+      cc: "healtcare@specialist.asia",
       subject: `🎁 Specialist Asia: ${voucher_name}`,
       html: `
         <div style="font-family: Arial, sans-serif; line-height: 1.6;">
