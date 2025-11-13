f8f0ec4e6d3b36ad8fc1b60d8e25e23cd910439d
Tra8der <ywlee8888@gmail.com>
2025-11-13 17:52:02 +0800

    Update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
+import GtagScript from "@/components/google/gtag";
 import "./globals.css";
 
 const geistSans = localFont({
@@ -28,6 +29,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <GtagScript />
         {children}
       </body>
     </html>
